63739cd737090a333b57bee9e586a4d0f936646c
Oumayma CHOUICHI <oumaymasaoudi6@gmail.com>
2026-02-17 16:08:29 +0100

    fix: utiliser SpringBootTest au lieu de WebMvcTest pour TicketControllerTest

--- src/test/java/com/hotel/tickethub/controller/TicketControllerTest.java
+++ src/test/java/com/hotel/tickethub/controller/TicketControllerTest.java
@@ -8,10 +8,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.*;
@@ -22,8 +23,9 @@ import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(TicketController.class)
+@SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
+@ActiveProfiles("test")
 class TicketControllerTest {
 
     @MockBean
